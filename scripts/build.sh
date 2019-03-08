632cdd786add2c76a7c66b486191f75b4ef33a0f
Thomas Pasch <thomas.pasch@siteos.de>
2019-03-08 10:47:20 +0100

    more scripts

---
+++ scripts/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ mvn -PIDE,dev -DskipTests clean package
---
+++ scripts/run-module-2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mv target/modules/spring-cloud-spring-service-connector-2.0.4.RELEASE.jar .
+
+/usr/lib/jvm/java-11/bin/java --module-path target/modules -jar target/modules/jhipster-sample-application-0.0.1-SNAPSHOT.jar --path-module spring.cloud.connectors.core=spring-cloud-spring-service-connector-2.0.4.RELEASE.jar
