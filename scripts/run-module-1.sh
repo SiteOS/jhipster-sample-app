9cffa608ef60b10cfe21cf5188ba49cfb82f4c9e
Thomas Pasch <thomas.pasch@siteos.de>
2019-03-08 11:19:08 +0100

    script fixes

--- scripts/run-module-1.sh
+++ scripts/run-module-1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/lib/jvm/java-11/bin/java --module-path target/modules -jar target/modules/jhipster-sample-application-0.0.1-SNAPSHOT.jar 
+/usr/lib/jvm/java-11/bin/java --module-path target/modules io.github.jhipster.sample/io.github.jhipster.sample.JhipsterSampleApplicationApp
 
