ed385edb0967230aa97641b40b1329c277be2546
Thomas Pasch <thomas.pasch@siteos.de>
2019-03-08 09:57:44 +0100

    fixed module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,42 @@
 open module io.github.jhipster.sample {
 
+    requires org.apache.commons.lang3;
+
+    requires org.apache.logging.slf4j;
     requires org.apache.logging.log4j;
+    requires slf4j.api;
+    requires logback.classic;
+
+    requires hibernate.core;
+    requires hibernate.envers;
+    requires hibernate.jpamodelgen;
+
+    requires liquibase.core;
+
+    requires spring.boot.actuator;
+    requires spring.security.core;
+    requires spring.security.config;
+    requires spring.security.web;
+
+    requires org.aspectj.weaver;
+
+    requires problem;
+    requires problem.violations;
+    requires problem.spring.web;
+
+    requires jhipster.framework;
+
     requires jackson.annotations;
+    requires jackson.datatype.problem;
+    requires com.fasterxml.jackson.datatype.hibernate5;
+    requires com.fasterxml.jackson.datatype.jdk8;
+    requires com.fasterxml.jackson.datatype.jsr310;
+    requires com.fasterxml.jackson.module.afterburner;
 
     requires cache.api;
 
     // requires activation;
+    requires java.annotation;
     requires java.instrument;
     requires java.persistence;
     requires java.sql;
@@ -14,6 +45,7 @@ open module io.github.jhipster.sample {
     requires java.xml.bind;
 
     requires org.hibernate.validator;
+    requires hibernate.commons.annotations;
 
     requires spring.beans;
     requires spring.boot;
